f69c84eb528c1aa0611acd8acaa3dd8ea7e25036
Elkin Arriero <e.arriero.g@gmail.com>
2015-08-05 23:28:55 -0500

    Removed description from todo lists and fix specs.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150804180212) do
+ActiveRecord::Schema.define(version: 20150806034134) do
 
   create_table "todo_items", force: :cascade do |t|
     t.integer  "todo_list_id"
@@ -25,7 +25,6 @@ ActiveRecord::Schema.define(version: 20150804180212) do
 
   create_table "todo_lists", force: :cascade do |t|
     t.string   "title"
-    t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
--- spec/factories.rb
+++ spec/factories.rb
@@ -11,7 +11,6 @@ FactoryGirl.define do
 
   factory :todo_list do
     title  "Todo List Title"
-    description "Todo List Description"
     user
   end
 end
